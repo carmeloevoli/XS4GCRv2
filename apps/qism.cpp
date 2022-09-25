ac553335f7b09ea000432864b3bc2cfb76df8293
carmeloevoli <carmelo.evoli@gssi.it>
2022-09-25 22:09:21 +0200

    plot antimatter

--- apps/qism.cpp
+++ apps/qism.cpp
@@ -21,9 +21,9 @@ void localFlux() {
   }
 }
 
-std::pair<double, double> Q_ISM_pbar(double E_s) {
+std::pair<double, double> Q_ISM_pbar(double E_s, SecondaryAntiprotonModels model) {
   XSECS xsecs;
-  xsecs.setSecondaryAntiprotons(SecondaryAntiprotonModels::AAFRAG);
+  xsecs.setSecondaryAntiprotons(model);
   auto xs = xsecs.createSecondaryAntiprotons();
 
   const auto flux_H = Boschini2017::H_LIS();
@@ -31,36 +31,82 @@ std::pair<double, double> Q_ISM_pbar(double E_s) {
 
   auto q_H = GSL::gslQAGIntegration<double>(
       [&](double lnEp) {
-        double E_p = std::exp(lnEp);
+        auto E_p = std::pow(10., lnEp);
         return E_p * flux_H.get(E_p) * xs->get(H1, TARGET::H, E_p, E_s);
       },
-      std::log(E_s), std::log(1e4 * E_s), 1000, 1e-3);
-  q_H *= 4. * M_PI;
+      std::log10(E_s), std::log10(1e4 * E_s), 1000, 1e-4);
 
   auto q_He = GSL::gslQAGIntegration<double>(
       [&](double lnEp) {
-        double E_p = std::exp(lnEp);
+        auto E_p = std::pow(10., lnEp);
         return E_p * flux_He.get(E_p) * xs->get(He4, TARGET::H, E_p, E_s);
       },
-      std::log(E_s), std::log(1e4 * E_s), 1000, 1e-3);
-  q_He *= 4. * M_PI;
+      std::log10(E_s), std::log10(1e4 * E_s), 1000, 1e-4);
 
   return {q_H, q_He};
+}
+
+std::pair<double, double> Q_ISM_pos(double E_s, SecondaryLeptonModels model) {
+  XSECS xsecs;
+  xsecs.setSecondaryLeptons(model);
+  auto xs = xsecs.createSecondaryLeptons(positron);
+
+  const auto flux_H = Boschini2017::H_LIS();
+  const auto flux_He = Boschini2017::He_LIS();
+
+  auto q_H = GSL::gslQAGIntegration<double>(
+      [&](double lnEp) {
+        auto E_p = std::pow(10., lnEp);
+        return E_p * flux_H.get(E_p) * xs->get(H1, TARGET::H, E_p, E_s);
+      },
+      std::log10(E_s), std::log10(1e4 * E_s), 1000, 1e-4);
+
+  auto q_He = GSL::gslQAGIntegration<double>(
+      [&](double lnEp) {
+        auto E_p = std::pow(10., lnEp);
+        return E_p * flux_He.get(E_p) * xs->get(He4, TARGET::H, E_p, E_s);
+      },
+      std::log10(E_s), std::log10(1e4 * E_s), 1000, 1e-4);
+
+  return {q_H, q_He};
+}
+
+double grammage(double E) {
+  auto value = 10. * cgs::gram / cgs::cm2;
+  value *= std::pow(E / 10. / cgs::GeV, -0.5);
+  return value / cgs::protonMass;
 }
 
 void Q_ISM() {
   std::ofstream outfile("output/qism.txt");
   outfile << "#E_s [GeV] - H->pbar - He->pbar - H->pos - He->pos - H->gamma - He->gamma\n";
   outfile << std::scientific;
-  auto secEnergy = UTILS::LogAxis(cgs::GeV, cgs::TeV, 10);
-  const double units = 1;
+  auto secEnergy = UTILS::LogAxis(cgs::GeV, cgs::TeV, 30);
+  const double units = 1. / cgs::GeV / cgs::second / cgs::steradian;
   for (auto E_s : secEnergy) {
     outfile << E_s / cgs::GeV << "\t";
-    auto q_pbar = Q_ISM_pbar(E_s);
-    outfile << q_pbar.first / units << "\t";
-    outfile << q_pbar.second / units << "\t";
-    // outfile << flux_H.get(T_n) / units << "\t";
-    // outfile << flux_He.get(T_n) / units << "\t";
+    std::cout << E_s / cgs::GeV << "\n";
+    outfile << grammage(E_s) / (1. / cgs::m2) << "\t";
+    {
+      auto q_pbar = Q_ISM_pbar(E_s, SecondaryAntiprotonModels::DIMAURO2014);
+      outfile << q_pbar.first / units << "\t";
+      outfile << q_pbar.second / units << "\t";
+    }
+    {
+      auto q_pbar = Q_ISM_pbar(E_s, SecondaryAntiprotonModels::WINKLER2017);
+      outfile << q_pbar.first / units << "\t";
+      outfile << q_pbar.second / units << "\t";
+    }
+    {
+      auto q_pos = Q_ISM_pos(E_s, SecondaryLeptonModels::KAMAE2006);
+      outfile << q_pos.first / units << "\t";
+      outfile << q_pos.second / units << "\t";
+    }
+    {
+      auto q_pos = Q_ISM_pos(E_s, SecondaryLeptonModels::ORUSA2022);
+      outfile << q_pos.first / units << "\t";
+      outfile << q_pos.second / units << "\t";
+    }
     outfile << "\n";
   }
 }
--- plots/plot_crflux.py
+++ plots/plot_crflux.py
@@ -16,10 +16,10 @@ def get_model(filename):
 def set_axes(fig, title):
     ax = fig.add_subplot(111)
     ax.set_xlim([.1, 1e6])
-    ax.set_ylim([1e1, 1e4])
+    #ax.set_ylim([1e1, 1e4])
     ax.set_xscale('log')
-    ax.set_xlabel(r'T [GeV/n]')
-    ax.set_ylabel(r'T$^{2.5}$ $\Phi$ [GeV/n$^{-1}$ m$^{-2}$ s$^{-1}$ sr$^{-1}$]')
+    ax.set_xlabel(r'E [GeV/n]')
+    ax.set_ylabel(r'E$^{2.5}$ $\Phi$ [GeV/n$^{-1}$ m$^{-2}$ s$^{-1}$ sr$^{-1}$]')
     ax.set_yscale('log')
     ax.set_title(title)
     return ax
@@ -32,17 +32,53 @@ def plot_CRDB_data(filename, slope, color):
 def plot_phe():
     fig = plt.figure(figsize=(10.5, 8))
     ax = set_axes(fig, '')
-#   ax.plot(T, inel * 100., color=color, label=label)
+    ax.set_xlim([1, 1e4])
+    ax.set_ylim([1e2, 4e4])
+    ax.set_ylabel(r'E$^{2.8}$ $\Phi$ [GeV/n$^{1.8}$ m$^{-2}$ s$^{-1}$ sr$^{-1}$]')
 
-    plot_CRDB_data('CRDB_H_AMS02_2015.txt', 2.5, 'tab:blue')
-    plot_CRDB_data('CRDB_He_AMS02_2017.txt', 2.5, 'tab:orange')
+    plot_CRDB_data('CRDB_H_AMS02_2015.txt', 2.8, 'tab:blue')
+    plot_CRDB_data('CRDB_He_AMS02_2017.txt', 2.8, 'tab:orange')
 
     T, H, He = get_model('output/lis.txt')
-    ax.plot(T, np.power(T, 2.5) * H, color='tab:red')
-    ax.plot(T, np.power(T, 2.5) * He, color='tab:green')
+    ax.plot(T, np.power(T, 2.8) * H, color='tab:purple', label='H')
+    ax.plot(T, np.power(T, 2.8) * He, color='tab:red', label='He')
+
+    ax.legend(fontsize=18)
+    savefig(plt, 'cr_phe')
+    
+def plot_antimatter():
+    fig = plt.figure(figsize=(10.5, 8))
+    ax = set_axes(fig, '')
+    ax.set_xlim([5, 1e3])
+    ax.set_ylim([1, 1e1])
+    ax.set_xlabel(r'E [GeV]')
+    ax.set_ylabel(r'E$^{2.8}$ $\Phi$ [GeV$^{1.8}$ m$^{-2}$ s$^{-1}$ sr$^{-1}$]')
+
+    plot_CRDB_data('CRDB_pbar_AMS02_2016.txt', 2.8, 'tab:blue')
+    plot_CRDB_data('CRDB_pos_AMS02_2019.txt', 2.8, 'tab:green')
+
+    T, X, qH, qHe = np.loadtxt('output/qism.txt', usecols=(0,1,2,3), unpack=True, skiprows=1)
+    ax.plot(T, np.power(T, 2.8) * (2.2 * X * qH), color='tab:purple', label=r'DiMauro $\bar p$')
+    #ax.plot(T, np.power(T, 2.8) * (2.2 * X * pbar_He), color='tab:blue', linestyle='--', label='qHe')
+
+    T, X, qH, qHe = np.loadtxt('output/qism.txt', usecols=(0,1,4,5), unpack=True, skiprows=1)
+    ax.plot(T, np.power(T, 2.8) * (2.0 * X * qH), color='tab:red', label=r'Winkler $\bar p$')
+    #ax.plot(T, np.power(T, 2.8) * (2.2 * X * pos_He), color='tab:orange', linestyle='--', label='qHe')
+
+    T, X, qH, qHe = np.loadtxt('output/qism.txt', usecols=(0,1,6,7), unpack=True, skiprows=1)
+    ax.plot(T, np.power(T, 2.8) * (2.0 * X * qH), color='tab:red', label=r'Kamae $e^+$')
+    #ax.plot(T, np.power(T, 2.8) * (2.2 * X * pos_He), color='tab:orange', linestyle='--', label='qHe')
+
+    T, X, qH, qHe = np.loadtxt('output/qism.txt', usecols=(0,1,8,9), unpack=True, skiprows=1)
+    ax.plot(T, np.power(T, 2.8) * (2.0 * X * qH), color='tab:red', label=r'Orusa $e^+$')
+    #ax.plot(T, np.power(T, 2.8) * (2.2 * X * pos_He), color='tab:orange', linestyle='--', label='qHe')
+
+
+
 
     ax.legend(fontsize=15)
-    savefig(plt, 'cr_phe')
-
+    savefig(plt, 'cr_antimatter')
+    
 if __name__== "__main__":
     plot_phe()
+    plot_antimatter()
