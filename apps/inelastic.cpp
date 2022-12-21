88d7902bda24b342059ca2b17aa3339944bbd5f5
Carmelo Evoli <carmelo.evoli@gssi.it>
2022-12-21 11:41:10 +0100

    remove unused function

--- apps/inelastic.cpp
+++ apps/inelastic.cpp
@@ -35,36 +35,10 @@ void get_inelastic_timescales(TotalInelasticModels model, std::string filename) 
   outfile.close();
 }
 
-void get_xsecs() {
-  XSECS xsec;
-  xsec.setTotalInelastic(TotalInelasticModels::GLAUBER);
-  const auto x_in = xsec.createTotalInelastic();
-  auto He = PID(2, 4);
-  auto C = PID(6, 12);
-  auto Fe = PID(26, 56);
-  auto particles = std::vector<PID>{He, C, Fe};
-
-  std::ofstream outfile("output/Glauber_sigmain.txt");
-  outfile << "#T_proj [GeV/n] - sigma_inelastic He, C, O, Fe [mbarn]\n";
-  outfile << std::scientific;
-
-  auto T_proj = UTILS::LogAxis(cgs::GeV, 1e4 * cgs::GeV, 1000);
-  for (auto T : T_proj) {
-    outfile << T / cgs::GeV << "\t";
-    for (auto p : particles) {
-      auto sigma = x_in->get(p, TARGET::H, T);
-      outfile << sigma / cgs::mbarn << "\t";
-    }
-    outfile << "\n";
-  }
-  outfile.close();
-}
-
 int main() {
   try {
     LOG::startup_information();
     {
-      // get_xsecs();
       get_inelastic_timescales(TotalInelasticModels::CROSEC, "output/CROSEC_tauin.txt");
       get_inelastic_timescales(TotalInelasticModels::TRIPATHI1999, "output/Tripathi1999_tauin.txt");
       get_inelastic_timescales(TotalInelasticModels::LETAW1983, "output/Letaw1983_tauin.txt");
