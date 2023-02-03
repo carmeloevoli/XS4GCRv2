09f68d75723c13b02ec34feb86aa77d7927767f0
Carmelo Evoli <carmelo.evoli@gssi.it>
2023-02-03 18:47:42 +0100

    last hermes tables

--- apps/hermesTables.cpp
+++ apps/hermesTables.cpp
@@ -9,16 +9,19 @@ int main() {
     LOG::startup_information();
     XSECS xsecs;
     xsecs.setPi0Gammas(Pi0GammaModels::AAFRAG);
-    auto neutralParticleType = NeutralParticleType::ALLNUS;
+    auto neutralParticleType = NeutralParticleType::GAMMA;
     auto xs = xsecs.createPi0Gammas(neutralParticleType);
-    auto xAxis = UTILS::LogAxis(1e-5, 1., 100);
-    auto TprojAxis = UTILS::LogAxis(1. * cgs::GeV, 100. * cgs::PeV, 200);
+    auto TprojAxis = UTILS::LogAxis(cgs::GeV, 1e8 * cgs::GeV, 8 * 128);
+    auto xAxis = UTILS::LogAxis(1e-5, 1., 6 * 128);
     const double units = cgs::mbarn / cgs::GeV;
-    std::string filename = "AAfrag2021Neutrino.txt";
+    std::string filename = "AAfrag2021Gamma.txt";
     std::ofstream outfile(filename);
     outfile << "#T_primary [GeV/n] - x - sigma_pp - sigma_pHe - sigma_Hep - sigma_HeHe [mbarn/GeV]\n";
     outfile << std::scientific;
+    size_t counter = 0;
     for (auto& Tproj : TprojAxis) {
+      std::cout << counter << " / " << TprojAxis.size() << "\n";
+      counter++;
       for (auto& x : xAxis) {
         outfile << Tproj / cgs::GeV << "\t" << x << "\t";
         outfile << xs->get(H1, TARGET::H, Tproj, x * Tproj) / units << "\t";
